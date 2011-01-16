bc61f354290a7f3ce7a079206e3228755a6cb8b5
void <git.email.address@googlemail.com>
2011-01-16 01:35:10 +0100

    Now using Network.Socket directly - works fine - no more AF_INET6 drama

--- Source/Veles/Server.hs
+++ Source/Veles/Server.hs
@@ -1,29 +1,42 @@
-module Server( runServer
-             ) where
+module Server(
+  runServer
+  ) where
 
-import Network
-import IO
 import Control.Concurrent
+import qualified Data.ByteString as DB
+import Data.ByteString.Char8 (unpack)
+import Network.Socket hiding (recv)
+import Network.Socket.ByteString (recv)
 
-data ConnectionInformation = ConnectionInformation { connectionHandle :: Handle
-                                                   , connectionHostName :: HostName
-                                                   , connectionPort :: PortNumber
-                                                   }
+data ConnectionInformation = ConnectionInformation {
+  connectionSocket :: Socket,
+  connectionAddress :: SockAddr
+  }
 
 runServer :: Int -> IO ()
 runServer port = withSocketsDo $ do
+  serverSocket <- socket AF_INET Stream defaultProtocol
+  serverAddress <- inet_addr "127.0.0.1"
+  bindSocket serverSocket $ SockAddrInet (fromIntegral port) serverAddress
+  listen serverSocket 1
   putStrLn $ "Listening on port " ++ (show port)
-  serverSocket <- listenOn $ PortNumber $ fromIntegral port
   sequence_ $ repeat $ acceptClient serverSocket
 
 acceptClient :: Socket -> IO ()
 acceptClient serverSocket = do
   putStrLn "Waiting for a new connection"
-  (handle, hostName, portNumber) <- accept serverSocket
-  putStrLn $ "New connection: " ++ hostName ++ ":" ++ (show portNumber)
-  forkIO $ clientThread $ ConnectionInformation handle hostName portNumber
+  (clientSocket, clientAddress) <- accept serverSocket
+  putStrLn $ "New connection: " ++ (show clientAddress)
+  forkIO $ processClient $ ConnectionInformation clientSocket clientAddress
   return ()
 
-clientThread :: ConnectionInformation -> IO ()
-clientThread client = do
-  return ()
+processClient :: ConnectionInformation -> IO ()
+processClient client = do
+  clientData <- recv (connectionSocket client) 0x1000
+  if DB.null clientData
+    then (putStrLn $ "Connection closed: " ++ clientAddress) >>
+         return ()
+    else (putStrLn $ "Received " ++ (show (DB.length clientData)) ++ " byte(s) from " ++ clientAddress ++ ": " ++ (unpack clientData)) >>
+         processClient client
+  where
+    clientAddress = (show (connectionAddress client))
