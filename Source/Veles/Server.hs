5e62626c2fc474d85682da4214d217a3db4ab2d0
void <git.email.address@googlemail.com>
2011-01-17 09:05:27 +0100

    Undid the old console interface

--- Source/Veles/Server.hs
+++ Source/Veles/Server.hs
@@ -4,13 +4,12 @@ module Veles.Server(
 
 import Control.Concurrent
 import Control.Monad
-import Control.Monad.Trans
 import qualified Data.ByteString as DB
 import Data.ByteString.Char8 (unpack)
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString (recv)
 
-import Knyaz.Console
+-- import Knyaz.Console
 
 data ConnectionInformation = ConnectionInformation {
   connectionSocket :: Socket,
@@ -18,14 +17,14 @@ data ConnectionInformation = ConnectionInformation {
   }
 
 -- run the SCGI server on the specified port, does not terminate
-runServer :: Int -> LockedConsole IO ()
-runServer port = lift . runLockedConsole . lift . withSocketsDo $ do
+runServer :: Int -> IO ()
+runServer port =  withSocketsDo $ do
   serverSocket <- socket AF_INET Stream defaultProtocol
   serverAddress <- inet_addr "127.0.0.1"
   bindSocket serverSocket $ SockAddrInet (fromIntegral port) serverAddress
   listen serverSocket 1
   putStrLn $ "Listening on port " ++ (show port)
-  sequence_ . repeat $ acceptClient serverSocket
+  forever $ acceptClient serverSocket
 
 -- accept a new client from a server socket and create a new thread to process it
 acceptClient :: Socket -> IO ()
