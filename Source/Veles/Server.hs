84d9b3572af64df432172165cd1e74dba2973036
void <git.email.address@googlemail.com>
2011-01-17 13:34:06 +0100

    Added the new console functions

--- Source/Veles/Server.hs
+++ Source/Veles/Server.hs
@@ -9,7 +9,7 @@ import Data.ByteString.Char8 (unpack)
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString (recv)
 
--- import Knyaz.Console
+import Knyaz.Console
 
 data ConnectionInformation = ConnectionInformation {
   connectionSocket :: Socket,
@@ -18,30 +18,33 @@ data ConnectionInformation = ConnectionInformation {
 
 -- run the SCGI server on the specified port, does not terminate
 runServer :: Int -> IO ()
-runServer port =  withSocketsDo $ do
+runServer port =  withSocketsDo . withLockedLinePrinting $ \printLine -> createServer printLine port
+
+createServer :: PrintFunction -> Int -> IO ()
+createServer printLine port = do
   serverSocket <- socket AF_INET Stream defaultProtocol
   serverAddress <- inet_addr "127.0.0.1"
   bindSocket serverSocket $ SockAddrInet (fromIntegral port) serverAddress
   listen serverSocket 1
-  putStrLn $ "Listening on port " ++ (show port)
-  forever $ acceptClient serverSocket
+  printLine $ "Listening on port " ++ (show port)
+  forever $ acceptClient printLine serverSocket
 
 -- accept a new client from a server socket and create a new thread to process it
-acceptClient :: Socket -> IO ()
-acceptClient serverSocket = do
-  putStrLn "Waiting for a new connection"
+acceptClient :: PrintFunction -> Socket -> IO ()
+acceptClient printLine serverSocket = do
+  printLine "Waiting for a new connection"
   (clientSocket, clientAddress) <- accept serverSocket
-  putStrLn $ "New connection: " ++ (show clientAddress)
-  void . forkIO . processClient $ ConnectionInformation clientSocket clientAddress
+  printLine $ "New connection: " ++ (show clientAddress)
+  void . forkIO . processClient printLine $ ConnectionInformation clientSocket clientAddress
   return ()
 
 -- handles the communication with a client in its own thread created by acceptClient
-processClient :: ConnectionInformation -> IO ()
-processClient client = do
+processClient :: PrintFunction -> ConnectionInformation -> IO ()
+processClient printLine client = do
   clientData <- recv (connectionSocket client) 0x1000
   if DB.null clientData
-    then putStrLn $ "Connection closed: " ++ clientAddress
-    else do putStrLn $ "Received " ++ (show $ DB.length clientData) ++ " byte(s) from " ++ clientAddress ++ ":  "  ++ (show $ unpack clientData)
-            processClient client
+    then printLine $ "Connection closed: " ++ clientAddress
+    else do printLine $ "Received " ++ (show $ DB.length clientData) ++ " byte(s) from " ++ clientAddress ++ ":  "  ++ (show $ unpack clientData)
+            processClient printLine client
   where
     clientAddress = show $ connectionAddress client
